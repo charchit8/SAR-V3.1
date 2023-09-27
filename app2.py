37cb7e23c68d3fc5fd1de6c60473ee2654bf7fc5
corprt <arti.chauhan@exlserivce.com>
2023-09-27 17:33:21 +0530

    changes

--- app2.py
+++ app2.py
@@ -1528,7 +1528,7 @@ with col_d2:
         
                 Question: {query}\n\
                 Context: {context_1}\n\                      
-                Response: '''
+                Response: (Give me a concise response in pointers. )'''
         
         
         response1 = usellm(prompt) 
