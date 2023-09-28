5f0fe1bad0e715707c1fff929f680afc7ca5ecae
corprt <arti.chauhan@exlserivce.com>
2023-09-28 22:13:55 +0530

    new commit

--- app2.py
+++ app2.py
@@ -931,10 +931,9 @@ with col2_up:
                     query = "Is this a Suspicious Activity?"
                     context_1 = docsearch.similarity_search(query, k=5)
                     prompt = f'''Act as a financial analyst and give concise answer to the question with given Context.
-                                 If transaction amount is above the $5,000 value threshold, check for below points to make sure it is a suspicious activity.
+                                 If transaction amount is above the $5,000 value threshold, check for below point to make sure it is a suspicious activity.
                                 1.Perform Name Enitity Recognition to identify names given in the context. Check if there is mention of any name other than the customer name. If your answer is No then it is not a suspicious activity, If Yes then it can be considered as a suspicious activity.
-                                2.Is there mention of any potential suspect name.
-                                Analyze above points properly and provide your response if it can be considered as a suspicious activity or not.\n\n\
+                                Analyze above point properly and provide your response if it can be considered as a suspicious activity or not.\n\n\
                                 Question: {query}\n\
                                 Context: {context_1}\n\                      
                                 Response: (Give me a concise response in pointers.)'''
