8d40dc8d35cf63d80d7468abbc6cb50eacf50e13
corprt <arti.chauhan@exlserivce.com>
2023-09-28 22:05:19 +0530

    new commit

--- app2.py
+++ app2.py
@@ -930,11 +930,11 @@ with col2_up:
                     #SARA Recommendation
                     query = "Is this a Suspicious Activity?"
                     context_1 = docsearch.similarity_search(query, k=5)
-                    prompt = f'''Act as a financial analyst and give concise answer if this can be reffered as suspicious activity or not, with given Context.
+                    prompt = f'''Act as a financial analyst and give concise answer to the question with given Context.
                                  If transaction amount is above the $5,000 value threshold, check for below points to make sure it is a suspicious activity.
-                                1.Check if the name mentioned in Invoice matches the customer name?If Yes then it is not a suspicious activity, If No then it can be considered as a suspicious activity.
-                                2.Is there mention of any potential suspect who used the card without the consent of the cardholder.\n\n\
-                                Analyze above points properly and provide your response if it can be considered as a suspicious activity or not.
+                                1.Perform Name Enitity Recognition to identify names given in the context. Check if there is mention of any name other than the customer name. If your answer is No then it is not a suspicious activity, If Yes then it can be considered as a suspicious activity.
+                                2.Is there mention of any potential suspect who used the card without the consent of the cardholder.
+                                Analyze above points properly and provide your response if it can be considered as a suspicious activity or not.\n\n\
                                 Question: {query}\n\
                                 Context: {context_1}\n\                      
                                 Response: (Give me a concise response in pointers.)'''
