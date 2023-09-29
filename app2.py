c55bd42de521e324dcadce12d2bb1c71db10d7a5
corprt <arti.chauhan@exlserivce.com>
2023-09-29 13:25:28 +0530

    new commit

--- app2.py
+++ app2.py
@@ -759,8 +759,8 @@ with col1_up:
             selected_file_path = os.path.join(directoty_path, fetched_pdf)
             text = convert_image_to_searchable_pdf(selected_file_path)
             file_name = os.path.basename(selected_file_path)
-            file_name = os.path.basename(file_name)
-            st.write(file_name)
+            x = file_name.split('.')
+            st.write(x)
             create_pdf(text,f'{file_name}.pdf')
 
         else:
@@ -780,7 +780,7 @@ with col1_up:
             pdf_files_ = pdf_files
         else: pass
 
-
+    st.write(temp_file_path)
 
 with col2_up:
          #This is the embedding model
