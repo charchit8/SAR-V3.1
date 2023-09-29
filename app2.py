0dbbbf33f3cf55b92b1b0fed7309796947d220ee
corprt <arti.chauhan@exlserivce.com>
2023-09-29 14:12:31 +0530

    new commit

--- app2.py
+++ app2.py
@@ -1601,7 +1601,7 @@ with col_d2:
             SAR refers to Suspicious activity Report, which is a document that financial institutions must file with the Financial Crimes Enforcement Network (FinCEN) based on the Bank Secrecy Act whenever there is a suspicious activity.\n\n\
             If transaction amount is above the $5,000 value threshold, you need to check below points to confirm this as a suspicious activity-
             1. Check for the customer name and address in Cardholder Information?
-            2. Check on whose name Invoice is billed. If that is different from customer's details.
+            2. Check on whose name Invoice is billed. If that is different from customer name.
             4. Any potential suspect is identified.\n\n\     
             If no suspicious activity is detected based on above mentioned points, write your response as - There is no indication of suspicious actuvity.Therefore,no requirement to file SAR with FinCEN.\n\n\
                     Question: {query}\n\
