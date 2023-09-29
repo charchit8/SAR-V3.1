2c80baea5c973b88db97f73b39930b0f50ea6145
corprt <arti.chauhan@exlserivce.com>
2023-09-29 17:07:21 +0530

    new commit

--- app2.py
+++ app2.py
@@ -1593,7 +1593,7 @@ with col_d2:
             # st.markdown("""<span style="font-size: 18px;">*2. There is an indication of suspicion with involvement of multiple individuals, mismatch of customer details on merchant invoice and identification of a potential suspect*.</span>""", unsafe_allow_html=True)           
                     
             query = "Is SAR filling required?"
-            context_1 = docsearch.similarity_search(query, k=7)
+            context_1 = docsearch.similarity_search(query, k=5)
             prompt = f'''Act as a financial analyst and give concise answer to the question, with given Context.\n\n\
             SAR refers to Suspicious activity Report, which is a document that financial institutions must file with the Financial Crimes Enforcement Network (FinCEN) based on the Bank Secrecy Act whenever there is a suspicious activity.\n\n\
             To confirm this as a suspicious activity-
