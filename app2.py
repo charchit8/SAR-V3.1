b9f8918436cbfb3903481e00491e705754806a4f
corprt <arti.chauhan@exlserivce.com>
2023-09-29 14:28:06 +0530

    new commit

--- app2.py
+++ app2.py
@@ -913,7 +913,7 @@ with col2_up:
                     query = "Is this a Suspicious Activity?"
                     context_1 = docsearch.similarity_search(query, k=5)
                     prompt = f'''Act as a financial analyst and give concise answer to the question as truthfully as possible with given Context.
-                                If transaction amount is above the $5,000 value threshold, then check for below point to make sure if it is a suspicious activity or not.
+                                Transaction amount is above the $5,000 value threshold, then check for below point to make sure if it is a suspicious activity or not.
                                 1.Check for the customer name and address in Cardholder Information?
                                 2.If you find any names and addresses that donot matches with customer details then it can be considered as a suspicious activity.
                                 3.If no names other than the customer are available then it is not a suspicous activity.
@@ -1604,7 +1604,7 @@ with col_d2:
             SAR refers to Suspicious activity Report, which is a document that financial institutions must file with the Financial Crimes Enforcement Network (FinCEN) based on the Bank Secrecy Act whenever there is a suspicious activity.\n\n\
             If transaction amount is above the $5,000 value threshold, you need to check below points to confirm this as a suspicious activity-
             1. Check for the customer name and address in Cardholder Information?
-            2. Check on whose name Invoice is billed. If that is different from customer name.
+            2. If you find any names and addresses that donot matches with customer details then it can be considered as a suspicious activity.
             4. Any potential suspect is identified.\n\n\     
             If no suspicious activity is detected based on above mentioned points, write your response as - There is no indication of suspicious actuvity.Therefore,no requirement to file SAR with FinCEN.\n\n\
                     Question: {query}\n\
