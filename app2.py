8c55581f1618a15070f8ddb3e4eb8301cccc3b9e
corprt <arti.chauhan@exlserivce.com>
2023-09-29 12:44:48 +0530

    new commit

--- app2.py
+++ app2.py
@@ -1607,7 +1607,7 @@ with col_d2:
             prompt = f'''Act as a financial analyst and give concise answer to the question, with given Context.\n\n\
             First Find out if there is any suspicious activity based on the following-
             1. Transaction amount is above the $5,000 value threshold.\n\
-            2. There is mismatch of customer details such as name,address on merchant invoice.
+            2. There is mention of other details that donot matches customer details such as name,address.
             3. Any potential suspect is identified.\n\n\     
             Based on the suspicious activity, answer if SAR filling is required or not.SAR refers to Suspicious activity Report, which is a document that financial institutions must file with the Financial Crimes Enforcement Network (FinCEN) based on the Bank Secrecy Act whenever there is a suspicious activity.\n\n\
             
