179c0c630468a73d90d4de6c87ff28c97c9e917c
corprt <arti.chauhan@exlserivce.com>
2023-09-29 13:40:05 +0530

    new commit

--- app2.py
+++ app2.py
@@ -738,7 +738,7 @@ with col1_up:
         elif file.endswith(file_ext2):
             st.write(file)
             text = convert_image_to_searchable_pdf(file)
-            file_name = file.split('.')
+            file_name = file.splittext('.')
             st.write(file_name)
             # st.write(split_name[0])
             create_pdf(text,f'{split_name[0]}.pdf') 
