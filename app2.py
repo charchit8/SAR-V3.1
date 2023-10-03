f98e53742ff085c7d827a4287e3ba6cb2ecca07b
corprt <arti.chauhan@exlserivce.com>
2023-10-03 12:15:44 +0530

    new commit

--- app2.py
+++ app2.py
@@ -307,7 +307,7 @@ if "pdf_files" not in st.session_state:
     st.session_state.pdf_files = []
 
 # reading files from local directory from fetch evidence button
-directoty_path = "data/"
+directoty_path = "data3/"
 fetched_files = read_pdf_files(directoty_path)
 
 
