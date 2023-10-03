f89ab210997a5a139d439b91ae8500203743cd55
corprt <arti.chauhan@exlserivce.com>
2023-10-03 13:42:05 +0530

    new commit

--- app2.py
+++ app2.py
@@ -916,13 +916,14 @@ with col2_up:
 
                     queries ="Please provide the following information from the context: If transaction,disputed amount is above the $5000 threshold,\
                               There is an indication of suspicion with involvement of multiple individuals whose details mismatch with customer details. (Customer details can be identified from Cardholder Information),\
-                              A potential suspect is identified, and based on the evidence, is this a suspicious activity (Summarize all the questions asked prior to this in a detailed manner),\
+                              A potential suspect is identified, Mention of an individual/suspect whose details such as name and address mismatch with customer details and based on the evidence, is this a suspicious activity (Summarize all the questions asked prior to this in a detailed manner),\
                               that is the answer of whether this is a suspicious activity"
                     
                     contexts = docsearch.similarity_search(queries, k=5) 
                     prompt = f" You are professional Fraud Analyst. Find answer to the questions as truthfully and in as detailed as possible as per given context only,\n\n\
                         1. If the transaction/disputed amount > 5,000 USD value threshold.\n\n\
-                        2. If a potential suspect is identified.\n\n\
+                        2. Is there mention of an individual/suspect whose details such as name,address mismatch with customer details. (Customer details can be identified from cardholder's information) \n\n\ 
+                        3. If a potential suspect is identified.\n\n\
                         Based the above findings, identify if this can be consider as Suspicious Activity or not.\n\n\
                         If transaction/disputed amount is < 5000 USD threshold and no suspicious activity is detected based on above mentioned points, write your response as - There is no indication of suspicious activity.\n\n\
                         Context: {contexts}\n\
