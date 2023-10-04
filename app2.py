35e144c76b25a7a835c224126116e02f5d6748a4
corprt <arti.chauhan@exlserivce.com>
2023-10-04 16:14:17 +0530

    new commit

--- app2.py
+++ app2.py
@@ -1601,8 +1601,8 @@ with col_d2:
         # Adding Radio button
              
         st.markdown("""<span style="font-size: 24px;color:#0000FF">Is SAR filing required?</span>""", unsafe_allow_html=True)
-        st.session_state.disabled = False
-        if st.session_state['sara_recommendation_gpt'] is not None:
+        if doc:
+        # if st.session_state['sara_recommendation_gpt'] is not None:
             st.write("#### *SARA Recommendation*")
             # st.markdown("""<span style="font-size: 18px;">*Based on the following findings for the underlying case, under Bank Secrecy Act, it is recommended to file this case as a suspicious activity:*</span>""", unsafe_allow_html=True)
             # st.markdown("""<span style="font-size: 18px;">*1. Transaction amount is above the $5,000 value threshold*</span>""", unsafe_allow_html=True)
@@ -1628,7 +1628,7 @@ with col_d2:
             st.warning('Please carefully review the recommendation and case details before the final submission',icon="⚠️")
  
         
-        elif st.session_state['sara_recommendation_llama'] is not None:
+        # elif st.session_state['sara_recommendation_llama'] is not None:
             query = "Give your recommendation if SAR filling is required or not?"
             context_1 = docsearch.similarity_search(query, k=5)
             prompt = f'''Act as a financial analyst and give concise answer to the question, with given Context.\n\n\
