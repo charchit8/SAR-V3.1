e88a048a5457ae15204dde07d52a451cf0f2897d
corprt <arti.chauhan@exlserivce.com>
2023-10-04 17:39:14 +0530

    new commit

--- app2.py
+++ app2.py
@@ -1602,8 +1602,13 @@ with col_d2:
              
         st.markdown("""<span style="font-size: 24px;color:#0000FF">Is SAR filing required?</span>""", unsafe_allow_html=True)
         
+        if tmp_table is not None:
+            st.write('True')
+        else:
+            st.write('False')
+
         if st.session_state['tmp_table_gpt'] is not None:
-            
+                       
             st.write("#### *SARA Recommendation*")
             # st.markdown("""<span style="font-size: 18px;">*Based on the following findings for the underlying case, under Bank Secrecy Act, it is recommended to file this case as a suspicious activity:*</span>""", unsafe_allow_html=True)
             # st.markdown("""<span style="font-size: 18px;">*1. Transaction amount is above the $5,000 value threshold*</span>""", unsafe_allow_html=True)
