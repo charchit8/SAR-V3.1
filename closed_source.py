93cf4361790ff49ecd6144dd97dc74b467d9e178
corprt <arti.chauhan@exlserivce.com>
2023-10-09 22:01:12 +0530

    new commit

--- closed_source.py
+++ closed_source.py
@@ -122,7 +122,7 @@ def generate_insights_gpt(temp_file_path):
             contexts = docsearch.similarity_search(query, k=5) 
             prompt = f" You are professional Fraud Analyst. Find answer to the questions as truthfully and in as detailed as possible as per given context only,\n\n\
                 If The transaction/disputed amount > 5,000 USD value threshold, then check below points to make sure if it is a suspicious activity or not: \n\n\
-                1. Check if invoice is billed customer? (Customer details can be identified from cardholder information).\n\n\
+                1. If invoice is billed to customer or not? (Customer is the cardholder)\n\n\
                 2. A potential suspect is identified? \n\n\
                 Even if transaction/disputed amount > 5,000 USD but other criteria does not met, then this can not be considered as a suspicious activity. \n\n\
                 Based on above points identify if this is a case of suspicious activity or not? \n\n\
