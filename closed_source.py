d23c8595e27dd405366dd9786eef1d38ccf1d10c
corprt <arti.chauhan@exlserivce.com>
2023-10-10 21:16:07 +0530

    new commit

--- closed_source.py
+++ closed_source.py
@@ -135,7 +135,7 @@ def generate_insights_gpt(temp_file_path):
             contexts = docsearch.similarity_search(query, k=5) 
             prompt = f" You are professional Fraud Analyst. Find answer to the questions as truthfully and in as detailed as possible as per given context only,\n\n\
                 If The transaction/disputed amount > 5,000 USD value threshold, then check below points to make sure if it is a suspicious activity or not: \n\
-                1. There is an indication of suspicion due to mismatch of cardholder details with details mentioned in merchant invoice (Perform name entity recoginition to identify name,address.) Also provide the mismatched details.\n\n\
+                1. {response_1} analyse this response,if details matches or not? If matches then there is no suspicion else, it can be a suspicipos activity.\n\n\
                 2. A potential suspect is identified? \n\n\
                 Even if transaction/disputed amount > 5,000 USD but if other criteria does not met, then this can not be considered as a suspicious activity. \n\n\
                 Based on above points identify if this is a case of suspicious activity or not? \n\n\
