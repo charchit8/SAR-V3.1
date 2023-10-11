a2d8a016e0f0aeb5f1ac8a97a778c572d3ef87e4
corprt <arti.chauhan@exlserivce.com>
2023-10-11 20:31:21 +0530

    New commit

--- utils.py
+++ utils.py
@@ -415,7 +415,7 @@ def merge_and_extract_text(pdf_list):
 llama_13b = HuggingFaceHub(
             repo_id="meta-llama/Llama-2-13b-chat-hf",
             model_kwargs={"temperature":0.01, 
-                        "min_new_tokens":100, 
+                        "min_new_tokens":500, 
                         "max_new_tokens":1000})
 
 
