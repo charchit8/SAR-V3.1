64f5ccb660886063106025a35242524286914a76
corprt <arti.chauhan@exlserivce.com>
2023-10-12 17:39:09 +0530

    new commit

--- open_source.py
+++ open_source.py
@@ -172,6 +172,7 @@ def generate_insights_llama(temp_file_path):
                 1. {response_3} analyse this response,if invoice is billed tp customer then there is no suspicion else, it can be a suspicipos activity. (kindly mention the mismatched details in your response).\n\n\
                 2. {response_4} analyse this response,If a potential suspect is identified then this can be a suspicious activity, else not.\n\n\
                 Even if transaction/disputed amount > 5,000 USD but if above criteria does not met, then this can not be considered as a suspicious activity. \n\n\
+                Analyse above points properly and identify if this is a case of suspicious activity or not? \n\n\
                 Context: {contexts}\n\
                 Response (Give me a concise response in few pointers only.)"
 
