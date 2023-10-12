15fa8b3c7662d4dfec07c546f1455b257796fbd7
corprt <arti.chauhan@exlserivce.com>
2023-10-12 15:33:28 +0530

    new commit

--- open_source.py
+++ open_source.py
@@ -40,7 +40,7 @@ def generate_insights_llama(temp_file_path):
             query = "What is the suspect's name?"
             context_1 = docsearch.similarity_search(query, k=5)
             prompt_1 =  f'''Act as a professional fraud analyst.You need to check the document and compare if any name discrepencies are present that points towards the suspect who used the card without the consent of the cardholder.
-                        Perform Name Enitity Recognition to identify the Suspect Name as accurately as possible, given the context. Suspect is the Person who has committed the fraud with the Customer, and misused the card.\n\n\
+                        Perform Name Enitity Recognition to identify the Suspect Name as accurately as possible, given the context. Suspect is the Person who has committed the fraud.\n\n\
                         Context: {context_1}\n\
                         Response: (Give a short response in a single sentence.Do not add any extra Note.)'''
             response = llama_llm(llama_13b,prompt_1)
