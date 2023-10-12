69b4d2def5bc7ee60db77984fe6a0f7b4da7163a
corprt <arti.chauhan@exlserivce.com>
2023-10-12 16:32:28 +0530

    new commit

--- open_source.py
+++ open_source.py
@@ -5,8 +5,8 @@ login(token=st.secrets["HUGGINGFACEHUB_API_TOKEN"])
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
 #This is the embedding model
-model_name = "thenlper/gte-small"
-# model_name = "sentence-transformers/all-MiniLM-L6-v2"
+# model_name = "thenlper/gte-small"
+model_name = "sentence-transformers/all-MiniLM-L6-v2"
 # model_name = "hkunlp/instructor-large" 
 
 def generate_insights_llama(temp_file_path):
