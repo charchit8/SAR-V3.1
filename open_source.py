2eaf7c960830ae0e51ea4e6866db599a7075293d
corprt <arti.chauhan@exlserivce.com>
2023-10-13 12:29:38 +0530

    new commit

--- open_source.py
+++ open_source.py
@@ -146,7 +146,7 @@ def generate_insights_llama(temp_file_path):
             prompt = f" You are professional Fraud Analyst. Find answer to the questions as truthfully and in as detailed as possible as per given context only,\n\n\
             Identify cardholder's name,adress from cardholder information. Customer is the person who is the owner of the card.\n\n\
             Identify name and address to whom merchant invoice is billed \n\n\
-            Once both the details are identified, compare both the details and check if details mentoned in the invoice matches customer details or not.\n\n\
+            Compare both the details identified above and check if invoice is billed to customer or not.\n\n\
                 Context: {contexts}\n\
                 Response (Give me a concise response.)"
             response_3 = llama_llm(llama_13b,prompt) 
@@ -169,7 +169,7 @@ def generate_insights_llama(temp_file_path):
             contexts = docsearch.similarity_search(query, k=5) 
             prompt = f" You are professional Fraud Analyst. Find answer to the questions as truthfully and in as detailed as possible as per given context only,\n\n\
                 If The transaction/disputed amount > 5,000 USD value threshold, then check below points to make sure if it is a suspicious activity or not: \n\
-                1. {response_3} analyse this response,if invoice is billed to customer then there is no suspicion else, it can be a suspicious activity. (kindly mention the mismatched details in your response).\n\n\
+                1. {response_3} analyse this response,if invoice is billed to cardholder then there is no suspicion else, it can be a suspicious activity. (kindly mention the mismatched details in your response).\n\n\
                 2. {response_4} analyse this response,If a potential suspect is identified then this can be a suspicious activity, else it is not a suspicious activity.\n\n\
                 Even if transaction/disputed amount > 5,000 USD but if above criteria does not met, then this can not be considered as a suspicious activity. \n\n\
                 Analyse above points properly and identify if this is a case of suspicious activity or not? \n\n\
