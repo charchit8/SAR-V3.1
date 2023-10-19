ddaa9ca4cbbc2d9887ac86a0224315085efcdcf8
corprt <arti.chauhan@exlserivce.com>
2023-10-19 23:27:51 +0530

    new commit

--- open_source.py
+++ open_source.py
@@ -342,7 +342,22 @@ def generate_insights_llama(temp_file_path):
     
     return st.session_state["tmp_table_llama"], st.session_state["sara_recommendation_llama"]
 
-# @st.cache_data(show_spinner=False)
+@st.cache_data(show_spinner=False)
+def summ_llama():
+    template = """Write a detailed summary of the text provided.
+    ```{text}```
+    Response: (Return your response in a single paragraph.) """
+    prompt = PromptTemplate(template=template,input_variables=["text"])
+    llm_chain_llama = LLMChain(prompt=prompt,llm=llama_13b)
+
+    summ_dict_llama = st.session_state.tmp_table_llama.set_index('Question')['Answer']
+    text = []
+    for key,value in summ_dict_llama.items():
+        text.append(value)
+    response_summ_llama = llm_chain_llama.run(text)
+    return response_summ_llama
+
+
 def summarize_llama():
 
     if 'clicked4' not in st.session_state:
@@ -356,18 +371,7 @@ def summarize_llama():
     summ_llama = st.button("Summarize",on_click=set_clicked4,disabled=st.session_state.disabled)
     with st.spinner("Summarize...."):
         if st.session_state.clicked4:
-            st.session_state.disabled=False
-            template = """Write a detailed summary of the text provided.
-            ```{text}```
-            Response: (Return your response in a single paragraph.) """
-            prompt = PromptTemplate(template=template,input_variables=["text"])
-            llm_chain_llama = LLMChain(prompt=prompt,llm=llama_13b)
-
-            summ_dict_llama = st.session_state.tmp_table_llama.set_index('Question')['Answer']
-            text = []
-            for key,value in summ_dict_llama.items():
-                text.append(value)
-            response_summ_llama = llm_chain_llama.run(text)
+            response_summ_llama = summ_llama()
             response_summ_llama = response_summ_llama.replace("$", " ")
             response_summ_llama = response_summ_llama.replace("5,000", "5,000 USD")
             response_summ_llama = response_summ_llama.replace("5,600", "5,600 USD")
