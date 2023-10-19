bad07ec46d6542577f6a6a5aa77a8fc02567b84d
corprt <arti.chauhan@exlserivce.com>
2023-10-19 16:57:07 +0530

    new commit

--- data.py
+++ data.py
@@ -135,7 +135,7 @@ def create_temp_file(directory_path,fetched_files):
         
     if st.session_state.pdf_files:
         for uploaded_file in st.session_state.pdf_files:
-            st.write(uploaded_file)
+            # st.write(uploaded_file)
             file_ext = tuple("pdf")
             if uploaded_file.name.endswith(file_ext):
                 file_pth = os.path.join(tmp_dir_, uploaded_file.name)
@@ -264,42 +264,37 @@ def pytesseract_code2(directory_path,fetched_files):
     # For uploaded files, reading files from the created direc and using pytesseract to convert
     # This is not working for images, but only for scanned pdfs
     for file in file_pth:
-        all_text = []
+        all_text = ""
         file_ext1 = tuple("pdf")
         file_ext2 = tuple(["png","jpeg"])
         if file.endswith(file_ext1):
             if is_searchable_pdf(file)==False:
                 text = convert_scanned_pdf_to_searchable_pdf(file)
-                all_text.append(text)
+                all_text += text
             else:
-                with pdfplumber.open(file) as pdf:
-                    for page in pdf.pages:
-                        text = page.extract_text()
-                        all_text.append(text)                    
+                for page in file.pages:
+                    all_text += page.extract_text()                   
         elif file.endswith(file_ext2):
             text = convert_image_to_searchable_pdf(file)
-            all_text.append(text)
+            all_text += text
         else:
             pass          
         
         
     #for fetched files, This is working for scanned pdf as well as images
     for fetched_pdf in fetched_files:
-        all_text = []
+        all_text = ""
         file_ext1 = tuple("pdf")
         file_ext2 = tuple(["png","jpeg"])
         if fetched_pdf.endswith(file_ext1):
             selected_file_path = os.path.join(directory_path, fetched_pdf)
             if is_searchable_pdf(selected_file_path)==False:
                 text = convert_scanned_pdf_to_searchable_pdf(selected_file_path)
-                all_text.append(text)
-     
+                all_text += text
             else:
                 file_pth = os.path.join(directory_path, fetched_pdf)
-                with pdfplumber.open(file_pth) as pdf:
-                    for page in pdf.pages:
-                        text = page.extract_text()
-                        all_text.append(text)
+                for page in file.pages:
+                    all_text += page.extract_text()  
         elif fetched_pdf.endswith(file_ext2):
             selected_file_path = os.path.join(directory_path, fetched_pdf)
             text = convert_image_to_searchable_pdf(selected_file_path)
